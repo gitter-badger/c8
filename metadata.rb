34b2717bd188f2737e7f34e5848640a7b2906961
Jacob Dearing <jacob.dearing@gmail.com>
2014-08-01 20:01:23 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# Encoding: utf-8
+name             'c8'
+maintainer       'Jacob Dearing'
+maintainer_email 'jacob.dearing@gmail.com'
+license          'MIT'
+description      'Installs/Configures c8'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
