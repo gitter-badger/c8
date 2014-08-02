bc9d04e3317ddf40384344f76ae58c3af5580537
Jacob Dearing <jacob.dearing@gmail.com>
2014-08-02 13:37:47 -0500

    bump release

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'jacob.dearing@gmail.com'
 license          'MIT'
 description      'Installs/Configures c8'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '1.0.0'
 
 depends 'apt'
 depends 'platformstack'
